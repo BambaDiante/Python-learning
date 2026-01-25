151fd03eb84cf3677186553267a43d4056dba25e
BambaDiante <ahmadoubambadiante4@gmail.com>
2026-01-25 02:24:34 +0000

    Utilisation des types de variables

--- fisrtcode.py
+++ fisrtcode.py
@@ -1,3 +1,7 @@
 print("This is my first python file");
 age=20
 print(age)
+print("Je teste si","les espaces viennent", "pour voir")
+#je remarque que si on met des expressions elles sont separes pa des espaces
+print("Affichage des types d'une variable")
+print(type(age))
